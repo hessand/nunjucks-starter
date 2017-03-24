596db20a4c9e2e40ddb27cb65a3fc7bc53bba29c
Andrew Hess <hess2@purdue.edu>
2017-03-24 22:16:00 +0000

    updates

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,28 @@
 module.exports = function(app) {
  
-    app.get('/route', function(req, res) {
+    /*app.get('/route', function(req, res) {
 	    res.render('index.html', {
 	        title : "Sample Node Express + Nunjucks app",
 	        body : "This is a route in another file"
 	    });
+	});*/
+	
+	app.get('/projects/:project', function(req, res){
+		console.log(req.params.project);
+		switch (req.params.project) {
+			case 'dataworks':
+				var data = require('./data/dataworks.json');
+				console.log('good switch');
+				res.render('index.html', {
+			        title : "Sample Node Express + Nunjucks app",
+			        body : "This is a route in another file",
+			        data: data
+			    });
+			    break;
+		    default:
+		    	console.log('bad switch');
+		    	res.redirect('/');
+		    	break;
+		}
 	});
 }
